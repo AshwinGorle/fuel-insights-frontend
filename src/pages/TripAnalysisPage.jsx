61dccb412345a11c7c21c69e7cf743582f2b836b
Bhishek-Parmar <itsbhishek@gmail.com>
2024-03-21 16:31:32 +0530

    trip analysis result fixed upto 2 decimal places

--- src/pages/TripAnalysisPage.jsx
+++ src/pages/TripAnalysisPage.jsx
@@ -66,7 +66,7 @@ const TripAnalysisPage = () => {
             <div className="w-full bg-white rounded-lg shadow dark:boxd16 md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
               <div className="p-6 space-y-4 md:px-16 md:space-y-6 sm:p-8 h-full">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Trip Analyis
+                  Trip Analysis
                 </h1>
 
                 <form className="space-y-4 md:space-y-6">
